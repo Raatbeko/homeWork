85367e18f16b17742b4b5f47d366053156025bdc
Dell <jumabekovradik@gmail.com>
2022-04-09 14:31:25 +0600

    for home work

---
+++ src/main/java/com/example/forhomeworks/ForhomeworksApplication.java
@@ -0,0 +1,13 @@
+package com.example.forhomeworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForhomeworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForhomeworksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forhomeworks/ForhomeworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forhomeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForhomeworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
